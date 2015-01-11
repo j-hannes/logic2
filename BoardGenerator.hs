8a5cd64ebe04b7e211e4149be4e9ee9b0640894f
Jay Hannes <Hannes_E@gmx.de>
2015-01-11 02:28:01 +0100

    Some more refactoring

--- BoardGenerator.hs
+++ BoardGenerator.hs
@@ -18,17 +18,18 @@ makeBoard (rows, columns) =
     }
   
 makeRow :: Int -> [Int] -> Row
-makeRow size blocks = Row {
+makeRow rowLength blocks = Row {
         Row.blocks = map (makeBlock margin) blocks
-      , Row.cells = replicate size Unknown
+      , Row.cells = replicate rowLength Unknown
       , Row.margin = margin
       , Row.hasOverlap = margin < maximum blocks
     }
   where
-    margin = calculateMargin size blocks
+    margin = calculateMargin rowLength blocks
     
 calculateMargin :: Int -> [Int] -> Int
-calculateMargin size blocks = size - sum blocks - length blocks + 1
+calculateMargin rowLength blocks =
+    rowLength - (sum blocks + length blocks - 1)
 
 makeBlock :: Int -> Int -> Block
 makeBlock margin size = Block size (size > margin)
